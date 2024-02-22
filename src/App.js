43c9a8d8c961924bf7d40f5d4ca2ec0210755906
Aakash Yadav <aakashya7@gmail.com>
2024-02-22 23:04:14 +0530

    learn useState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import { useState } from "react";
+
 function App() {
+    const[advice, setAdvice] = useState("");
 
     async function getAdvice(){
         const res = await fetch('https://api.adviceslip.com/advice');
         const data = await res.json();
-        console.log(data.slip.advice);
+        setAdvice(data.slip.advice);
     }
 
     return(
     <div>
         <h1>Hello! React</h1>
+        <h2>{advice}</h2>
         <button onClick = {getAdvice}>Get Advice</button>
     </div>
     );
